6394aa6d995996285f55edd36432fc9d6a68ce8e
Harsh Dixit <159412945+harshdixitcodes@users.noreply.github.com>
2024-05-11 19:57:33 +0530

    Update Problem_2.cpp

--- Problem_2.cpp
+++ Problem_2.cpp
@@ -1,3 +1,4 @@
+// First Approach
 #include<iostream>
 using namespace std;
 
@@ -21,3 +22,37 @@ int main() {
 
     return 0;  // Return 0 to indicate successful completion
 }
+
+// Another Possible Approach
+/*
+#include<iostream>  // Include the IO stream library for input and output operations
+
+using namespace std;  // Use the standard namespace to avoid prefixing std:: to standard library entities
+
+int main(){
+    unsigned long sum = 0;  // Initialize 'j' to store the sum of even Fibonacci numbers
+    unsigned long temp;   // Temporary variable to hold the previous Fibonacci number during swap
+    unsigned long i = 1;  // Start the first Fibonacci number
+    unsigned long prev = 1;  // Initialize 'prev' to the zeroth Fibonacci number, which is technically 1 here
+
+    // Loop until the Fibonacci number exceeds 4 million
+    while (i <= 4000000)
+    {
+        temp = i;  // Store the current Fibonacci number in 'temp'
+        i = i + prev;  // Generate the next Fibonacci number
+        prev = temp;  // Update 'prev' to the former current Fibonacci number
+
+        // Check if the new Fibonacci number is even
+        if (i % 2 == 0)
+        {
+            j += i;  // Add it to 'j' if it is even
+        }
+        
+    }
+
+    // Output the result to the console
+    cout << "The sum of even-valued terms in the Fibonacci sequence not exceeding 4 million is: " << j << endl;
+
+    return 0;  // Return 0 to indicate successful completion
+}
+*/
